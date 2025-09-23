96003b8b06ae472e4145712eaa172b3715787806
Felix Widström <felix.widstrom@hotmail.se>
2025-09-24 01:21:40 +0200

    Removed a project and hid a project until its reworked.

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,4 +1,3 @@
-import HouseCalc from '$assets/img/housecalc.png';
 import PantryTracker from '$assets/img/pantrytracker.png';
 import Viewer from '$assets/img/3dviewer.png';
 
@@ -11,22 +10,16 @@ export type Project = {
 
 export const projects: Array<Project> = [
 	{
-		title: 'Housing Calculator',
-		description: 'A calculator that helps users estimate the cost of building a house. Done in collaboration with Devolv as a school project.',
-		image: HouseCalc,
-		link: 'https://devolv-project.pages.dev/forum/intro'
-	},
-	{
 		title: 'SL-Widget',
 		description: 'An android application that provides the user with a simple to use widget for quickly seeing the coming departures from an SL stop location.',
 		link: 'https://github.com/felixwidstrom/SL-Widget'
 	},
-	{
-		title: 'Pantry Tracker',
-		description: 'An application that helps users keep track of their pantry items and their expiration dates.',
-		image: PantryTracker,
-		link: 'https://felixwidstrom.github.io/Pantry-Tracker'
-	},
+	// {
+	// 	title: 'Pantry Tracker',
+	// 	description: 'An application that helps users keep track of their pantry items and their expiration dates.',
+	// 	image: PantryTracker,
+	// 	link: 'https://felixwidstrom.github.io/Pantry-Tracker'
+	// },
 	// {
 	// 	title: '3D Viewer',
 	// 	description: '',
